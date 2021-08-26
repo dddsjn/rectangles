e78012b45620b9f9a334ec4d27dcb0c6a2d2977f
Jonathan Dodds <jonathan.dodds@virginwines.co.uk>
2021-08-26 16:30:06 +0100

    Added can_hold function

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,22 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.length
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.length > other.length
+    }
+
 }
 
 fn main() {
 
-    let rect1 = Rectangle {
-        width: 50,
-        length: 40,
-    };
+    let rect1 = Rectangle { width: 50, length: 40, };
+    let rect2 = Rectangle { width: 45, length: 38, };
+    let rect3 = Rectangle { width: 45, length: 48, };
 
     println!(" The width is {}",rect1.width );
     println!(" The length is {}",rect1.length );
     println!(" The area is {}",rect1.area() );
-
+    println!(" Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2) );
+    println!(" Can rect1 hold rect3 ? {}", rect1.can_hold(&rect3) );
 }
